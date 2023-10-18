664398c56a6c587967a9d09e3ab701907e3a4e15
FARHAN-27 <farhantanvir0434@gmail.com>
2023-10-18 14:24:58 +0600

    Initial commit

--- app/src/main/java/com/example/bottom_navigation_view_tutorial/login.java
+++ app/src/main/java/com/example/bottom_navigation_view_tutorial/login.java
@@ -1,14 +1,26 @@
 package com.example.bottom_navigation_view_tutorial;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class login extends AppCompatActivity {
-
+    AppCompatButton logInBtn ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        logInBtn = findViewById(R.id.logInBtn) ;
+
+        logInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class) ;
+                startActivity(intent);
+            }
+        });
     }
 }
